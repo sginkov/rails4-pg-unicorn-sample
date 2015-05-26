ccf40d6f6cf25330c918b1d68dce72c5bc9538d8
sginkov <s.ginkov@gmail.com>
2015-05-26 11:58:57 +0300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4PgUnicornSample::Application.config.secret_key_base = 'bf9e21862abf75aed87e79b644d72d11a16396c5fb753b761742845008a862e1668bfeb212c0dc0dfcfc7a114f3ab776cecb41ebcab136ded0488478491efde6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails4PgUnicornSample::Application.config.session_store :cookie_store, key: '_rails4-pg-unicorn-sample_session'
